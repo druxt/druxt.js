2856d1a55e14033a259cdbbdcfd40af53d7b36d7
Stuart Clark <stuart@realityloop.com>
2020-02-27 18:57:50 +1100

    [#1] Added deserializer relationships support.

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -6,8 +6,33 @@ export default (context, inject) => {
   const options = {}
 
   <% if (options.JSONAPIDeserializer) { %>
+  // JSONAPIDeserialzer.
   options.preprocessEntity = async resource => {
-    const results = await new Deserializer({}).deserialize({ data: [resource.data.data ]})
+    const options = <%= JSON.stringify(options.JSONAPIDeserializer) %>
+
+    // Build map of relationships.
+    for (const key in resource.data.data.relationships) {
+      if (!resource.data.data.relationships[key].data) {
+        continue
+      }
+
+      const data = Array.isArray(resource.data.data.relationships[key].data)
+        ? resource.data.data.relationships[key].data
+        : [resource.data.data.relationships[key].data]
+
+        for (const relationship of data) {
+          if (typeof options[relationship.type] !== 'undefined') {
+            continue
+          }
+
+          options[relationship.type] = {
+            valueForRelationship: relationship => relationship
+          }
+        }
+    }
+
+    const results = await new Deserializer(options).deserialize({ data: [resource.data.data ]})
+
     return results[0]
   }
   <% } %>
