1c329b0ff2ebd7589dbd06a2b597fb99f0d902ca
Stuart Clark <stuart@realityloop.com>
2020-05-04 14:37:05 +1000

    [#1] Updated schema import path.

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,5 +1,5 @@
 export default (context, inject) => {
   inject('druxtSchema', {
-    import: async id => import(`~/.nuxt/schemas/${id}.json`).then(m => m.default || m)
+    import: async id => import(`./schemas/${id}.json`).then(m => m.default || m)
   })
 }
