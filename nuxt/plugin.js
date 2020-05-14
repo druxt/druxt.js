21b5ec1bb2308afdd23d89e1929b8747a01cfa50
Stuart Clark <stuart@realityloop.com>
2020-05-15 08:45:54 +1000

    [#22] Removed JSON API Deserializer from plugin.

--- nuxt/plugin.js
+++ nuxt/plugin.js
@@ -1,5 +1,4 @@
 import { DruxtRouter } from 'druxt-router'
-<% if (options.router.JSONAPIDeserializer) { %>import { Deserializer } from 'jsonapi-serializer'<% } %>
 
 export default (context, inject) => {
   const baseUrl = '<%= options.baseUrl %>'
@@ -8,38 +7,6 @@ export default (context, inject) => {
   <% if (typeof options.axios === 'object') { %>
   // Axios settings.
   options.axios = <%= JSON.stringify(options.axios) %>
-  <% } %>
-
-  <% if (options.router.JSONAPIDeserializer) { %>
-  // JSONAPIDeserialzer.
-  options.preprocessEntity = async resource => {
-    const options = <%= JSON.stringify(options.router.JSONAPIDeserializer) %>
-
-    // Build map of relationships.
-    for (const key in resource.data.data.relationships) {
-      if (!resource.data.data.relationships[key].data) {
-        continue
-      }
-
-      const data = Array.isArray(resource.data.data.relationships[key].data)
-        ? resource.data.data.relationships[key].data
-        : [resource.data.data.relationships[key].data]
-
-        for (const relationship of data) {
-          if (typeof options[relationship.type] !== 'undefined') {
-            continue
-          }
-
-          options[relationship.type] = {
-            valueForRelationship: relationship => relationship
-          }
-        }
-    }
-
-    const results = await new Deserializer(options).deserialize({ data: [resource.data.data ]})
-
-    return results[0]
-  }
   <% } %>
 
   const router = new DruxtRouter(baseUrl, options)
