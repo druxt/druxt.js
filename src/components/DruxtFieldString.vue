c71d6c355bbfff415ef1156faae3a34572686afe
Stuart Clark <stuart@realityloop.com>
2020-05-21 15:25:52 +1000

    [#1] Added basic fields.

---
+++ src/components/DruxtFieldString.vue
@@ -0,0 +1,20 @@
+<template>
+  <span>{{ value }}</span>
+</template>
+
+<script>
+import { DruxtFieldMixin } from '../mixins/field'
+
+export default {
+  name: 'DruxtFieldString',
+
+  mixins: [DruxtFieldMixin],
+
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,9 @@ export { DruxtEntity }
 
 import DruxtField from './DruxtField.vue'
 export { DruxtField }
+
+import DruxtFieldString from './DruxtFieldString.vue'
+export { DruxtFieldString }
+
+import DruxtFieldTextDefault from './DruxtFieldTextDefault.vue'
+export { DruxtFieldTextDefault }
