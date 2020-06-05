df3c46879c5eb8cb7a7d890b272fe858ba72e60f
Stuart Clark <stuart@realityloop.com>
2020-06-05 18:27:39 +1000

    [#1] Updated test coverage.

--- src/components/__tests__/DruxtView.test.js
+++ src/components/__tests__/DruxtView.test.js
@@ -1,20 +1,36 @@
 import { createLocalVue, mount } from '@vue/test-utils'
+import Vuex from 'vuex'
 
 import { DruxtView } from '..'
 
 // Setup local vue instance.
 const localVue = createLocalVue()
+localVue.use(Vuex)
+
+const mocks = {
+  $druxtRouter: () => ({
+    getResource: jest.fn().mockResolvedValue({})
+  })
+}
+const stubs = ['druxt-entity']
+
+let store
 
 const mountComponent = () => {
   const propsData = {
     display: 'page_1',
     view: 'featured_articles'
   }
-  return mount(DruxtView, { localVue, propsData })
+  return mount(DruxtView, { localVue, mocks, propsData, stubs })
 }
 
 describe('Component - DruxtView', () => {
-  test('pages', async () => {
+  beforeEach(() => {
+    // Setup vuex store.
+    store = new Vuex.Store()
+  })
+
+  test('default', async () => {
     const wrapper = mountComponent()
 
     expect(wrapper.vm).toHaveProperty('display')
