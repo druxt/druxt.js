5bbb9755b303c04adde019a42dd453e11d7a815d
Stuart Clark <stuart@realityloop.com>
2020-06-13 17:36:55 +1000

    [#1] Added support for wrapper component and view mode.

--- src/components/DruxtView.vue
+++ src/components/DruxtView.vue
@@ -1,12 +1,21 @@
 <template>
-  <div v-if="results">
+  <component
+    :is="component"
+    v-if="view && results"
+    v-bind="props"
+  >
+    <template v-slot:header v-if="headers">
+      <span v-for="header of headers" :key="header.id" v-html="header.content.value" />
+    </template>
+
     <druxt-entity
       v-for="result of results"
       :key="result.id"
       :type="result.type"
       :uuid="result.id"
+      :mode="mode"
     />
-  </div>
+  </component>
 </template>
 
 <script>
@@ -14,27 +23,100 @@ export default {
   name: 'DruxtView',
 
   props: {
-    display: {
+    display_id: {
       type: String,
       default: 'default',
     },
 
-    view: {
+    type: {
+      type: String,
+      default: 'view--view'
+    },
+
+    uuid: {
+      type: String,
+      required: true
+    },
+
+    view_id: {
       type: String,
       required: true
     }
   },
 
   data: () => ({
-    results: false
+    results: false,
+    view: false
   }),
 
   created() {
-    const query = { type: `views--${this.view}`, id: this.display }
+    const viewQuery = { type: this.type, id: this.uuid }
+    this.$druxtRouter().getResource(viewQuery).then(view => {
+      this.view = view
+    })
 
-    this.$druxtRouter().getResource(query).then(results => {
+    const resultsQuery = { type: `views--${this.view_id}`, id: this.display_id }
+    this.$druxtRouter().getResource(resultsQuery).then(results => {
       this.results = results
     })
+  },
+
+  computed: {
+    component() {
+      for (const suggestion of this.suggestions) {
+        if (typeof this.$options.components[suggestion] !== 'undefined') {
+          return suggestion
+        }
+      }
+
+      return 'div'
+    },
+
+    display() {
+      if (!this.view) return false
+
+      if (this.display_id === 'default') return this.view.attributes.display[this.display_id]
+
+      return {
+        ...this.view.attributes.display[this.display_id],
+        ...this.view.attributes.display['default']
+      }
+    },
+
+    headers() {
+      if (!this.view) return false
+
+      return this.display.display_options.header
+    },
+
+    mode() {
+      if (!this.view) return false
+
+      return this.display.display_options.row.options.view_mode
+    },
+
+    props() {
+      if (this.component === 'div') return false
+
+      return {
+        view: this.view,
+        results: this.results
+      }
+    },
+
+    suggestions() {
+      const suggestions = []
+
+      const prefix = 'DruxtView'
+
+      const viewId = this.view_id.split('_').map(part => part.charAt(0).toUpperCase() + part.slice(1)).join('')
+      const displayId = this.display_id.split('_').map(part => part.charAt(0).toUpperCase() + part.slice(1)).join('')
+
+      suggestions.push(prefix + viewId + displayId)
+      suggestions.push(prefix + viewId)
+
+      return suggestions
+    },
   }
 }
 </script>
