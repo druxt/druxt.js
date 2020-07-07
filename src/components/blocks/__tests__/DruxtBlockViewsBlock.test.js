5ccb3b70e3cae389a29c910e621c78bc28e22b68
Stuart Clark <stuart@realityloop.com>
2020-07-07 13:19:09 +1000

    [#7] Updated tests.

--- src/components/blocks/__tests__/DruxtBlockViewsBlock.test.js
+++ src/components/blocks/__tests__/DruxtBlockViewsBlock.test.js
@@ -6,6 +6,15 @@ import { DruxtBlockViewsBlock } from '..'
 const localVue = createLocalVue()
 const stubs = ['druxt-view']
 
+// @TODO - Mock axios and add fixtures.
+const mocks = {
+  $druxtRouter: () => ({
+    getResources: jest.fn().mockImplementation(() => Promise.resolve([{
+      id: 'uuid'
+    }]))
+  })
+}
+
 const mockBlock = {
   id: 'test-block',
   attributes: {
@@ -18,12 +27,16 @@ const mockBlock = {
 const mountComponent = (entity, options = {}) => {
   const propsData = { block: entity }
 
-  return mount(DruxtBlockViewsBlock, { localVue, propsData, stubs, ...options })
+  return mount(DruxtBlockViewsBlock, { localVue, mocks, propsData, stubs, ...options })
 }
 
 describe('Component - DruxtBlockViewsBlock', () => {
   test('default', async () => {
     const wrapper = mountComponent(mockBlock)
+
+    await localVue.nextTick()
+
+    expect(wrapper.vm.uuid).toBe('uuid')
 
     expect(wrapper.vm.props).toStrictEqual({
       displayId: 'display_id',
