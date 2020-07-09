31188dc957819f242989d87c2ac96bb06e22d05e
Stuart Clark <stuart@realityloop.com>
2020-07-09 13:33:19 +1000

    [#13] Added DruxtBlockSystemBreadcrumbBlock.

---
+++ src/components/blocks/DruxtBlockSystemBreadcrumbBlock.vue
@@ -0,0 +1,13 @@
+<template>
+  <druxt-breadcrumb />
+</template>
+
+<script>
+import { DruxtBlocksBlockMixin } from 'druxt-blocks'
+
+export default {
+  name: 'DruxtBlockSystemBreadcrumbBlock',
+
+  mixins: [DruxtBlocksBlockMixin],
+}
+</script>
