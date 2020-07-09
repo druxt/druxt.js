f2853d6e19ecbc02d3aea331c95600730b0bae67
Stuart Clark <stuart@realityloop.com>
2020-07-09 12:36:30 +1000

    [#10] Added Page title block.

---
+++ src/components/blocks/DruxtBlockPageTitleBlock.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+import { DruxtBlocksBlockMixin } from '../../mixins/block'
+
+export default {
+  name: 'DruxtBlockPageTitleBlock',
+
+  mixins: [DruxtBlocksBlockMixin],
+
+  computed: {
+    ...mapState('druxtRouter', {
+      title: state => state.route.label
+    })
+  }
+}
+</script>
