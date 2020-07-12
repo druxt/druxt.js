e16a2cd7ab4a40ea072d3047783ef72b049c8da6
Stuart Clark <stuart@realityloop.com>
2020-07-12 19:15:41 +1000

    [#18] Added DruxtBlockSystemMenuBlock.

---
+++ src/components/blocks/DruxtBlockSystemMenuBlock.vue
@@ -0,0 +1,19 @@
+<template>
+  <druxt-menu :name="name" />
+</template>
+
+<script>
+import { DruxtBlocksBlockMixin } from 'druxt-blocks'
+
+export default {
+  name: 'DruxtBlockSystemMenuBlock',
+
+  mixins: [DruxtBlocksBlockMixin],
+
+  computed: {
+    name() {
+      return this.settings.id.split(':')[1]
+    },
+  },
+}
+</script>
