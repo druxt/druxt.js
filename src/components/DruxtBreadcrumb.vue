446cec1a6ba948a1468431d648b61ca3f7f846c5
Stuart Clark <stuart@realityloop.com>
2020-08-25 12:43:10 +1000

    [#24] Changed to ‘fetch’ for nojs support.

--- src/components/DruxtBreadcrumb.vue
+++ src/components/DruxtBreadcrumb.vue
@@ -24,15 +24,50 @@ export default {
     }
   },
 
+  async fetch() {
+    // Reset items.
+    this.items = {}
+
+    // If there is no route, stop here.
+    if (!this.route || !Object.keys(this.route).length) return
+
+    // Home crumb.
+    if (this.settings.home) {
+      this.items['/'] = {
+        to: '/',
+        text: 'Home'
+      }
+    }
+
+    // If we are at the root of the site, stop here.
+    if (this.$route.path === '/') return
+
+    // Current route crumb.
+    this.items[this.$route.path] = {
+      text: this.route.label
+    }
+
+    // Add crumbs for route parents.
+    const paths = this.$route.path.split('/').filter(String)
+    paths.pop()
+    while (paths.length > 0) {
+      const to = '/' + paths.join('/')
+
+      const route = await this.getRoute(to)
+      if (route.label) {
+        this.items[to] = { to, text: route.label }
+      }
+
+      paths.pop()
+    }
+  },
+
   data: () => ({
-    loading: 0,
     items: {},
   }),
 
   computed: {
     crumbs() {
-      if (!!this.loading) return []
-
       return Object.keys(this.items).sort((a, b) => a.length - b.length).map(key => {
         return this.items[key]
       })
@@ -67,64 +102,7 @@ export default {
     })
   },
 
-  watch: {
-    '$route': 'getItems'
-  },
-
-  created() {
-    this.getItems()
-  },
-
   methods: {
-    getItems() {
-      // Reset items.
-      this.items = {}
-
-      // If there is no route, stop here.
-      if (!this.route || !Object.keys(this.route).length) return
-
-      // Home crumb.
-      if (this.settings.home) {
-        this.items['/'] = {
-          to: '/',
-          text: 'Home'
-        }
-      }
-
-      // If we are at the root of the site, stop here.
-      if (this.$route.path === '/') return
-
-      // Current route crumb.
-      this.items[this.$route.path] = {
-        text: this.route.label
-      }
-
-      // Add crumbs for route parents.
-      const paths = this.$route.path.split('/').filter(String)
-      paths.pop()
-      while (paths.length > 0) {
-        this.loading++
-        const to = '/' + paths.join('/')
-        this.items[to] = {}
-
-        this.getRoute(to).then((route) => {
-          this.loading--
-
-          delete this.items[to]
-          if (route.label) {
-            this.items[to] = { to, text: route.label }
-          }
-
-          this.$forceUpdate()
-        }).catch(error => {
-          this.loading--
-          delete this.items[to]
-        })
-
-        paths.pop()
-      }
-    },
-
     ...mapActions({
       getRoute: 'druxtRouter/getRoute'
     })
