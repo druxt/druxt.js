b9a0e37ffca654512e82a03c84013b4df90c31b8
Stuart Clark <stuart@realityloop.com>
2020-08-29 15:22:05 +1000

    [#59] Updated fetch method for live docs.

--- src/components/DruxtEntity.vue
+++ src/components/DruxtEntity.vue
@@ -74,13 +74,10 @@ export default {
   /**
    * Nuxt.js fetch method.
    *
-   * Invokes DruxtRouterEntityMixin and DruxtSchemaMixin fetch methods.
-   *
    * @see {@link https://nuxtjs.org/api/pages-fetch/}
    */
   async fetch() {
-    await DruxtRouterEntityMixin.fetch.call(this)
-    await DruxtSchemaMixin.fetch.call(this)
+    await this.fetch()
   },
 
   /**
@@ -218,7 +215,22 @@ export default {
     tokenType: () => 'entity'
   },
 
+  created() {
+    // Workaround for Vuepress docs.
+    if (!this.entity || !this.schema) {
+      this.fetch()
+    }
+  },
+
   methods: {
+    /**
+     * Invokes DruxtRouterEntityMixin and DruxtSchemaMixin fetch methods.
+     */
+    async fetch() {
+      await DruxtRouterEntityMixin.fetch.call(this)
+      await DruxtSchemaMixin.fetch.call(this)
+    },
+
     /**
      * Checks if an Entity field is empty.
      *
