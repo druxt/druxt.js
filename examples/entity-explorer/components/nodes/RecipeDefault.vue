973acd2356fc01faa324947ba34f08271f5a4c86
Stuart Clark <stuart@realityloop.com>
2020-10-10 16:34:27 +1100

    [#23] Added entity explorer example.

---
+++ examples/entity-explorer/components/nodes/RecipeDefault.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card class="pa-2" outlined>
+    <h2>{{ entity.attributes.title }}</h2>
+    <slot />
+  </v-card>
+</template>
+
+<script>
+import { DruxtEntityMixin } from 'druxt-entity'
+export default {
+  mixins: [DruxtEntityMixin]
+}
+</script>
