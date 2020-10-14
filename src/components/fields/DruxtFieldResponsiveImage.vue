4d023b3f331fa5dd5e5f349041dba095df0d6076
Stuart Clark <stuart@realityloop.com>
2020-10-14 17:57:32 +1100

    [#71] Fixed HMR image bug.

--- src/components/fields/DruxtFieldResponsiveImage.vue
+++ src/components/fields/DruxtFieldResponsiveImage.vue
@@ -1,7 +1,7 @@
 <template>
   <component
     :is="wrapper.component"
-    v-if="entities && typeof entities[0] !== 'undefined'"
+    v-if="!$fetchState.pending"
     v-bind="wrapper.props"
   >
     <!-- Label: Above -->
