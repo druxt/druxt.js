eb0bf1f79167f176bbba3f0e98a2b9deec04bf6b
Stuart Clark <stuart@realityloop.com>
2020-12-16 07:59:45 +1100

    [#13] Added default DruxtBlockRegion based output.

--- src/components/DruxtSite.vue
+++ src/components/DruxtSite.vue
@@ -1,11 +1,25 @@
 <template>
-  <component :is="component.is" v-bind="component.propsData">
-    <Nuxt />
+  <component
+    :is="component.is"
+    v-bind="component.propsData"
+  >
+    <template
+      v-for="region of regions"
+      #[region]
+    >
+      <DruxtBlockRegion
+        :key="region"
+        :name="region"
+        :theme="theme"
+      />
+    </template>
   </component>
 </template>
 
 <script>
+import { DrupalJsonApiParams } from 'drupal-jsonapi-params'
 import { DruxtComponentMixin } from 'druxt'
+import { mapActions } from 'vuex'
 
 /**
  * @example @lang vue
@@ -19,8 +33,40 @@ export default {
 
   mixins: [DruxtComponentMixin],
 
+  props: {
+    theme: {
+      type: String,
+      required: true
+    }
+  },
+
+  async fetch() {
+    const resourceType = 'block--block'
+    const regions = await this.getResources({
+      resource: resourceType,
+      query: new DrupalJsonApiParams()
+        .addFilter('theme', this.theme)
+        .addFields(resourceType, ['region']),
+    }).then((resources) => resources.map((resource) => resource.attributes.region).filter((v, i, s) => s.indexOf(v) === i))
+    this.regions = regions
+
+    await DruxtComponentMixin.fetch.call(this)
+  },
+
+  data: () => ({
+    regions: []
+  }),
+
+  methods: {
+    ...mapActions({ getResources: 'druxtRouter/getResources' }),
+  },
+
   druxt: ({ vm }) => ({
-    componentOptions: [['default']]
+    componentOptions: [[vm.theme], ['default']],
+
+    propsData: {
+      theme: vm.theme
+    }
   })
 }
 </script>
