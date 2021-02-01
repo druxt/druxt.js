954090ae8604588fbe8ede8dc5e2e92fdd1b4213
Stuart Clark <stuart@realityloop.com>
2021-02-01 12:00:26 +1100

    [#13] Added ability to use custom config.

--- bin/druxt-docgen.js
+++ bin/druxt-docgen.js
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
 
+const yargs = require('yargs')
 const DruxtDocgen = require('..')
-const druxtDocgen = new DruxtDocgen()
+
+const argv = yargs
+  .option('config', {
+    alias: 'c',
+    description: 'Path to config',
+    type: 'string',
+  })
+  .help()
+  .alias('help', 'h')
+  .argv
+
+const druxtDocgen = new DruxtDocgen(argv.config || null)
 
 const main = async function () {
   // Generate documentation.
