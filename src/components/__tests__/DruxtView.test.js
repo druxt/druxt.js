20536f229129cbbe13b25d4d831d26b858aee74e
Stuart Clark <stuart@realityloop.com>
2021-03-19 09:44:44 +1100

    [#48] Updated test coverage.

--- src/components/__tests__/DruxtView.test.js
+++ src/components/__tests__/DruxtView.test.js
@@ -11,19 +11,18 @@ localVue.use(Vuex)
 // Setup mock Vuex store.
 let store
 
-const mountComponent = (propsData) => {
-  const mocks = {
-    $fetch: jest.fn(),
-    $fetchState: {
-      pending: true
-    },
-    $route: {
-      query: {}
-    }
+const mocks = {
+  $createElement: jest.fn(),
+  $fetch: jest.fn(),
+  $fetchState: {
+    pending: true
+  },
+  $route: {
+    query: {}
   }
-
-  return mount(DruxtView, { localVue, mocks, propsData, store })
 }
+
+const mountComponent = (propsData) => mount(DruxtView, { localVue, mocks, propsData, store })
 
 describe('DruxtView', () => {
   beforeEach(() => {
@@ -53,20 +52,38 @@ describe('DruxtView', () => {
     // Computed.
     expect(wrapper.vm.attachments_after).toStrictEqual([])
     expect(wrapper.vm.attachments_before).toStrictEqual([])
+    expect(wrapper.vm.filters).toStrictEqual([])
     expect(wrapper.vm.headers).toStrictEqual([])
     expect(wrapper.vm.mode).toBe('card')
 
-    // DruxtComponentMixin.
+    // DruxtModule.
     expect(wrapper.vm.component.is).toBe('DruxtWrapper')
     expect(wrapper.vm.component.options).toStrictEqual([
       'DruxtViewFeaturedArticlesPage1',
       'DruxtViewFeaturedArticles'
     ])
+    expect(Object.keys(wrapper.vm.getScopedSlots())).toStrictEqual([
+      'header',
+      'attachments_before',
+      'results',
+      'pager',
+      'attachments_after',
+      'default'
+    ])
 
-    // Pagination.
-    wrapper.vm.model.page = 1
-    expect(wrapper.vm.query).toStrictEqual({ page: 1 })
+    // Query.
+    wrapper.vm.model = {
+      page: 1,
+      filter: { test: 1 },
+      sort: 'test'
+    }
+    expect(wrapper.vm.query).toStrictEqual({
+      page: 1,
+      'views-filter': { test: 1 },
+      'views-sort[sort_by]': 'test',
+    })
     expect(wrapper.vm.showPager).toBe(true)
+    expect(wrapper.vm.showSorts).toBe(false)
 
     wrapper.vm.$route.query = {}
     await localVue.nextTick()
@@ -77,9 +94,49 @@ describe('DruxtView', () => {
     })
     expect(wrapper.vm.query).toStrictEqual({})
 
-    // Sorting.
-    wrapper.vm.model.sort = 'test'
-    expect(wrapper.vm.query).toStrictEqual({ 'views-sort[sort_by]': 'test' })
-    expect(wrapper.vm.showSorts).toBe(false)
+    // Watches.
+    expect(mocks.$fetch).toHaveBeenCalledTimes(1)
+    await wrapper.vm.$options.watch.displayId.call(mocks)
+    expect(mocks.$fetch).toHaveBeenCalledTimes(2)
+    await wrapper.vm.$options.watch.query.call(mocks)
+    expect(mocks.$fetch).toHaveBeenCalledTimes(3)
+    await wrapper.vm.$options.watch.uuid.call(mocks)
+    expect(mocks.$fetch).toHaveBeenCalledTimes(4)
+  })
+
+  test('recipes--embed', async () => {
+    const wrapper = mountComponent({
+      displayId: 'embed_1',
+      uuid: 'f6c38097-d534-4bfb-87d9-09526fe44e9c',
+      viewId: 'recipes'
+    })
+    await wrapper.vm.$options.fetch.call(wrapper.vm)
+
+    // Scoped slots.
+    const scopedSlots = wrapper.vm.getScopedSlots.call(mocks)
+    expect(Object.keys(scopedSlots)).toStrictEqual([
+      'header',
+      'filters',
+      'sorts',
+      'attachments_before',
+      'results',
+      'pager',
+      'attachments_after',
+      'default'
+    ])
+
+    const mockSlots = DruxtView.methods.getScopedSlots.call({
+      $createElement: jest.fn(),
+      headers: wrapper.vm.headers,
+      filters: wrapper.vm.filters,
+      results: wrapper.vm.results,
+    })
+    for (const key of Object.keys(mockSlots).filter((key) => key !== 'default')) {
+      mockSlots[key] = jest.fn()
+    }
+    mockSlots.default({ foo: 'bar' })
+    expect(mockSlots.header).toBeCalledWith({ foo: 'bar' })
+    expect(mockSlots.filters).toBeCalledWith({ foo: 'bar' })
+    expect(mockSlots.results).toBeCalledWith({ foo: 'bar' })
   })
 })
