a07f57d0c2cc6e4410ecc5d0abea0d4a21aaab7f
Stuart Clark <stuart@realityloop.com>
2021-03-29 09:40:34 +1100

    [#67] Updated storybook for DruxtEntityForm.

--- nuxt/druxt-entity.stories.js
+++ nuxt/druxt-entity.stories.js
@@ -1,52 +1,70 @@
-import { DruxtEntity } from 'druxt-entity'
+import { DrupalJsonApiParams } from 'drupal-jsonapi-params'
+import { DruxtClient } from 'druxt'
+import { default as DruxtEntity } from 'druxt-entity/src/components/DruxtEntity'
+import { default as DruxtEntityForm } from 'druxt-entity/src/components/DruxtEntityForm'
+
+const druxt = new DruxtClient('<%= options.druxt.baseUrl %>', <%= devalue(options.druxt) %>)
 
 export default {
   title: '<%= options.title %>',
   component: DruxtEntity,
+  subcomponents: { DruxtEntityForm },
   argTypes: {
     mode: {
-      control: null,
-      description: 'The Entity display mode.',
-      table: {
-        defaultValue: { summary: 'default' },
-      },
+      control: false,
     },
     type: {
-      control: null,
-      description: 'The JSON:API resource type.',
+      control: false,
     },
     uuid: {
       control: {
         type: 'select',
-        options: [<%= options.uuids.map((s) => `'${s}'`).join(', ') %>],
+        options: [<%= (options.entities || []).map((o) => `'${o.id}'`).join(', ') %>],
       },
-      description: 'The Entity UUID.',
-      type: { required: true },
-    }
+    },
   },
 }
 
-const Template = (args, { argTypes }) => {
+const ViewTemplate = (args, { argTypes }) => {
+  args.type = '<%= options.resourceType %>'
+  args.uuid = args.uuid || '<%= ((options.entities || [])[0] || {}).id %>'
   return {
     props: Object.keys(args),
     template: '<DruxtEntity v-bind="$props" v-on="$props" />',
   }
 }
 
-<% for (mode of options.modes) { %>
-export const <%= mode.charAt(0).toUpperCase() + mode.slice(1) %> = Template.bind({})
-<%= mode.charAt(0).toUpperCase() + mode.slice(1) %>.args = {
-  mode: '<%= mode %>',
-  type: '<%= options.type %>',
-  uuid: '<%= options.uuids[0] %>',
-}
-<%= mode.charAt(0).toUpperCase() + mode.slice(1) %>.storyName = '<%= mode %>'
-<%= mode.charAt(0).toUpperCase() + mode.slice(1) %>.parameters = {
-  docs: {
-    source: {
-      code: '<DruxtEntity\n  mode="<%= mode %>"\n  type="<%= options.type %>"\n  uuid="<%= options.uuids[0] %>"\n/>'
-    }
+const FormTemplate = (args, ctx) => {
+  args.type = '<%= options.resourceType %>'
+  args.uuid = args.uuid || '<%= ((options.entities || [])[0] || {}).id %>'
+  return {
+    props: Object.keys(args),
+    template: '<DruxtEntityForm v-bind="$props" v-on="$props" />',
   }
 }
 
-<% } %>
+//<% for (mode of options.viewDisplays) { %>
+export const View<%= mode.charAt(0).toUpperCase() + mode.slice(1) %> = ViewTemplate.bind({})
+View<%= mode.charAt(0).toUpperCase() + mode.slice(1) %>.storyName = 'View: <%= mode %>'
+View<%= mode.charAt(0).toUpperCase() + mode.slice(1) %>.args = { mode: '<%= mode %>' }
+View<%= mode.charAt(0).toUpperCase() + mode.slice(1) %>.parameters = {
+  docs: {
+    source: {
+      code: '<DruxtEntity\n  mode="<%= mode %>"\n  type="<%= options.resourceType %>"\n  uuid=""\n/>'
+    }
+  }
+}
+//<% } %>
+
+//<% for (mode of options.formDisplays) { %>
+export const Form<%= mode.charAt(0).toUpperCase() + mode.slice(1) %> = FormTemplate.bind({})
+Form<%= mode.charAt(0).toUpperCase() + mode.slice(1) %>.storyName = 'Form: <%= mode %>'
+Form<%= mode.charAt(0).toUpperCase() + mode.slice(1) %>.args = { mode: '<%= mode %>' }
+Form<%= mode.charAt(0).toUpperCase() + mode.slice(1) %>.parameters = {
+  docs: {
+    source: {
+      code: '<DruxtEntityForm\n  mode="<%= mode %>"\n  type="<%= options.resourceType %>"\n  uuid=""\n/>'
+    }
+  }
+}
+//<% } %>
--- src/nuxtStorybook.js
+++ src/nuxtStorybook.js
@@ -16,58 +16,74 @@ export default async function ({ stories }) {
 
   const druxt = new DruxtClient(options.druxt.baseUrl, options.druxt)
 
-  const resourceType = 'entity_view_display--entity_view_display'
-  const query = new DrupalJsonApiParams()
-    .addFilter('status', 1)
-    .addFields(resourceType, ['bundle', 'mode', 'targetEntityType'])
-  const collections = await druxt.getCollectionAll(resourceType, query)
+  // Fetch and process schemas.
+  const [view, form] = (await Promise.all(['view', 'form'].map(async (type) =>
+    await druxt.getCollectionAll(`entity_${type}_display--entity_${type}_display`, new DrupalJsonApiParams()
+      .addFilter('status', 1)
+      .addFields(`entity_${type}_display--entity_${type}_display`, [
+        'bundle',
+        'mode',
+        'targetEntityType',
+      ])
+    )))).map((schema) => schema.map((collection) => collection.data.map((o) => o)).flat())
+  const schemas = [...view, ...form]
 
-  // Build Entity data.
-  const entities = {}
-  for (const collection of collections) {
-    for (const display of collection.data) {
-      const { bundle, mode, targetEntityType } = display.attributes
-
-      entities[targetEntityType] = entities[targetEntityType] || {}
-      entities[targetEntityType][bundle] =
-        entities[targetEntityType][bundle] || []
-      entities[targetEntityType][bundle].push(mode)
-    }
-  }
-
-  // Write Entity stories.
-  for (const entityType of Object.keys(entities)) {
-    for (const bundle of Object.keys(entities[entityType])) {
-      const modes = Array.from(new Set(entities[entityType][bundle]))
-
-      // Ensure 'Default' is the first item.
-      modes.sort((a) => (a === 'default' ? -1 : 0))
-
-      const title = titleFn(['Druxt Entity', entityType, bundle])
-
-      const type = `${entityType}--${bundle}`
-
-      let uuids = []
-      try {
-        const query = new DrupalJsonApiParams()
-          .addFilter('status', 1)
-          .addFields(type, ['id'])
-        const resources = await druxt.getCollection(type, query)
-        uuids = resources.data.map((resource) => resource.id)
-      } catch (e) {}
-
-      if (uuids.length) {
-        addTemplate({
-          src: resolve(__dirname, '../nuxt/druxt-entity.stories.js'),
-          fileName: `stories/druxt-entity.${type}.stories.js`,
-          options: { modes, title, type, uuids },
-        })
-      }
-    }
-  }
-
-  // Add stories.
-  stories.push(
-    resolve(options.buildDir, './stories/druxt-entity.*.stories.js')
+  // Get Entity type and bundle data.
+  const entityTypes = Array.from(new Set(schemas.map((o) => o.attributes.targetEntityType)))
+    .map((entity) => ({
+      entity,
+      bundles: Array.from(new Set(schemas.filter((o) => o.attributes.targetEntityType === entity).map((o) => o.attributes.bundle))),
+    })
   )
+
+  // Fetch Entities for UUID controls.
+  const entities = await Promise.all([
+    ...entityTypes.map(({ entity, bundles }) => bundles.map(async (bundle) => {
+      try {
+        return {
+          resourceType: `${entity}--${bundle}`,
+          entities: (await druxt.getCollection(`${entity}--${bundle}`, new DrupalJsonApiParams()
+            .addFilter('status', 1)
+            .addFields(`${entity}--${bundle}`, ['id', 'title', 'name', 'info'])
+          )).data.map((o) => ({
+            id: o.id,
+            title: (o.attributes || {}).title || (o.attributes || {}).name || (o.attributes || {}).info || o.id,
+          })),
+        }
+      }
+      catch(e) {
+        return {
+          resourceType: `${entity}--${bundle}`,
+          entites: [],
+        }
+      }
+    })).flat()
+  ])
+
+  // Build Entity Stories.
+  const entityStories = entityTypes.map(({ entity, bundles }) => bundles.map((bundle) => {
+    const viewDisplays = view.filter((o) => o.attributes.targetEntityType === entity && o.attributes.bundle === bundle).map((o) => o.attributes.mode).sort((a) => (a === 'default' ? -1 : 0))
+    const formDisplays = form.filter((o) => o.attributes.targetEntityType === entity && o.attributes.bundle === bundle).map((o) => o.attributes.mode).sort((a) => (a === 'default' ? -1 : 0))
+
+    const resourceType = `${entity}--${bundle}`
+    
+    const title = titleFn(['Druxt Entity', entity, bundle])
+
+    addTemplate({
+      src: resolve(__dirname, '../nuxt/druxt-entity.stories.js'),
+      fileName: `stories/druxt-entity.${resourceType}.stories.js`,
+      options: {
+        druxt: options.druxt,
+        entities: entities.find((o) => o.resourceType === resourceType).entities,
+        formDisplays,
+        resourceType,
+        title,
+        viewDisplays
+      },
+    })
+
+    return resolve(options.buildDir, `./stories/druxt-entity.${resourceType}.stories.js`)
+  })).flat()
+
+  stories.push.apply(stories, entityStories)
 }
