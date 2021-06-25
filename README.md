e8fd3f93e73abf73a0636caf61b43cb338bb53ab
Stuart Clark <stuart@realityloop.com>
2021-06-25 15:37:46 +1000

    [#114] Updated documentation.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+<div style="width: 50%; margin: 0 auto 2em; text-align: center;">
+
+![](https://druxtjs.org/logo.svg)
+
 # DruxtEntity
 
 [![npm](https://badgen.net/npm/v/druxt-entity)](https://www.npmjs.com/package/druxt-entity)
@@ -5,7 +9,11 @@
 [![Known Vulnerabilities](https://snyk.io/test/github/druxt/druxt-entity/badge.svg?targetFile=package.json)](https://snyk.io/test/github/druxt/druxt-entity?targetFile=package.json)
 [![codecov](https://codecov.io/gh/druxt/druxt-entity/branch/develop/graph/badge.svg)](https://codecov.io/gh/druxt/druxt-entity)
 
+</div>
+
+
 > The DruxtEntity module provides a Drupal Display Mode powered Entity, Form and Field component system for your Nuxt.js application.
+
 
 ## Links
 
@@ -13,11 +21,13 @@
 - Documentation: https://entity.druxtjs.org
 - Community Discord server: https://discord.druxtjs.org
 
+
 ## Install
 
 `$ npm install druxt-entity`
 
-## Usage
+
+### Nuxt.js
 
 Add module to `nuxt.config.js`
 
@@ -36,6 +46,59 @@ module.exports = {
 }
 ```
 
+
+## Usage
+
+### DruxtEntity component
+
+The DruxtEntity component renders content entities using Drupal's View display modes.
+
+```vue
+<DruxtEntity :type="resourceType" :uuid="uuid" :mode="displayMode" />
+```
+
+![Example DruxtEntity component](https://raw.githubusercontent.com/druxt/druxt-entity/HEAD/docs/images/druxt-entity.png)
+
+See the [DruxtEntity API Documentation](https://entity.druxtjs.org/api/components/DruxtEntity.html) for more information.
+
+
+### DruxtEntityForm component
+
+The DruxtEntityForm component uses Drupal's Form displays modes for content creation and editing.
+
+```vue
+<DruxtEntityForm :type="resourceType" :mode="displayMode" />
+```
+
+![Example DruxtEntityForm component](https://raw.githubusercontent.com/druxt/druxt-entity/HEAD/docs/images/druxt-entity-form.png)
+
+See the [DruxtEntityForm API Documentation](https://entity.druxtjs.org/api/components/DruxtEntity.html) for more information.
+
+
+### Theming
+
+Both components can be themed by providing a default template:
+```vue
+<DruxtEntity type="node--page" :uuid="uuid">
+  <template #default="{ entity, fields, schema }">
+    {{ entity }}
+  </template>
+</DruxtEntity>
+```
+
+The module also provides Wrapper components with scoped slots for theming:
+```vue
+<template>
+  <div>
+    <h1>{{ $attrs.entity.attributes.title }}</h1>
+    <slot name="body" />
+  <div>
+</template>
+```
+
+See the [Druxt Theming guide](https://druxtjs.org/guide/theming.html) for more information.
+
+
 ## Options
 
 ### Druxt Entity options
@@ -46,6 +109,7 @@ These options are specific to this module.
 | --- | --- | --- | --- | --- |
 | `entity.query.fields` | `string[]` | No | `[]` | An array of fields to filter all Entity JSON:API queries. |
 | `entity.query.schema` | `boolean` | No | `false` | Whether to automatically filter fields based on Display schema. |
+
 
 ### Base Druxt options
 
--- src/components/DruxtField.vue
+++ src/components/DruxtField.vue
@@ -2,11 +2,9 @@
 import { DruxtModule } from 'druxt'
 
 /**
- * The `<DruxtField />` Vue.js component.
- *
- * - Renders the provided Field data.
- * - Supports Component Suggestion based theming with Vue.js Slots.
- *
+ * The `<DruxtField />` component renders an Entity Field for the DruxtEntity
+ * and DruxtEntityForm components.
+ * 
  * @example
  * <DruxtField
  *   data="A wholesome pasta bake is the ultimate comfort food. This delicious bake is super quick to prepare and an ideal midweek meal for all the family."
@@ -30,7 +28,7 @@ export default {
     /**
      * JSON:API errors.
      *
-     * @type {array}
+     * @type {object[]}
      */
     errors: {
       type: Array,
@@ -59,6 +57,8 @@ export default {
 
     /**
      * Field options.
+     * 
+     * @todo deprecate this?
      *
      * @type {object}
      * @default {}
@@ -86,6 +86,9 @@ export default {
     return [...parts, getCounter(parts.join(':'))].join(':')
   },
 
+  /**
+   * @property {object} model - The model object.
+   */
   data: ({ value }) => ({
     model: value,
   }),
@@ -117,15 +120,47 @@ export default {
     },
   },
 
+  /** DruxtModule settings */
   druxt: {
+    /**
+     * Provides the available component naming options for the Druxt Wrapper.
+     *
+     * @param {object} context - The module component ViewModel.
+     * @returns {ComponentOptions}
+     */
     componentOptions: ({ schema }) => ([
       [schema.type || 'undefined', schema.id, (schema.config || {}).schemaType],
       [schema.type || 'undefined', (schema.config || {}).schemaType],
       ['default', (schema.config || {}).schemaType],
     ]),
 
+    /**
+     * Provides propsData for the DruxtWrapper.
+     *
+     * @param {object} context - The module component ViewModel.
+     * @returns {PropsData}
+     */
     propsData: ({ errors, model, relationship, schema }) => ({ errors, relationship, schema, value: model }),
 
+    /**
+     * Provides the scoped slots object for the Module render function.
+     *
+     * A scoped slot is provided for the label, as well as label-above and
+     * label-inline depending on the field schema.
+     * 
+     * A default slot is provided with debug information if Nuxt is in
+     * development mode.
+     *
+     * @example <caption>DruxtField**SchemaType**.vue</caption> @lang vue
+     * <template>
+     *   <div>
+     *     <slot name="label" />
+     *     {{ $attrs.value }}
+     *   </div>
+     * </template>
+     *
+     * @return {ScopedSlots} The Scoped slots object.
+     */
     slots(h) {
       const scopedSlots = {}
 
@@ -166,4 +201,74 @@ export default {
     },
   },
 }
+
+/**
+ * Provides the available naming options for the Wrapper component.
+ *
+ * @typedef {array[]} ComponentOptions
+ *
+ * @example @lang js
+ * [
+ *   'DruxtField[Type][Id][SchemaType]',
+ *   'DruxtField[Type][Id]
+ *   'DruxtField[Type][SchemaType]',
+ *   'DruxtField[Default][SchemaType]',
+ *   'DruxtField[Type]',
+ *   'DruxtField[Default]',
+ * ]
+ *
+ * @example <caption>Media field</caption> @lang js
+ * [
+ *   'DruxtFieldEntityReferenceEntityViewFieldMediaImageView',
+ *   'DruxtFieldEntityReferenceEntityViewFieldMediaImage',
+ *   'DruxtFieldEntityReferenceEntityViewView',
+ *   'DruxtFieldDefaultView',
+ *   'DruxtFieldEntityReferenceEntityView',
+ *   'DruxtFieldDefault',
+ * ]
+ */
+
+/**
+ * Provides propsData for use in the Wrapper component.
+ *
+ * @typedef {object} PropsData
+ * @param {object[]} errors - JSON:API errors.
+ * @param {boolean} relationship - Field relationship status.
+ * @param {object} schema - Drupal Field schema information.
+ * @param {object} value - The Field value.
+ *
+ * @example @lang js
+ * {
+ *   errors: [],
+ *   relationship: true,
+ *   schema: {
+ *     config: {},
+ *     description: '',
+ *     id: 'field_media_image',
+ *     label: {},
+ *     required: true,
+ *     settings: {},
+ *     thirdPartySettings: [],
+ *     type: 'entity_reference_entity_view',
+ *     weight: 4,
+ *   },
+ *   value: {
+ *     data: {
+ *       id: '71a90e86-55b6-40fe-b77d-552dca1bee77',
+ *       type: 'media--image',
+ *     },
+ *     links: {},
+ *   },
+ * }
+ */
+
+/**
+ * Provides scoped slots for use in the Wrapper component.
+ *
+ * @typedef {object} ScopedSlots
+ * @param {function} label - The field label.
+ * @param {function} label-above - The field label, if label position is 'above'.
+ * @param {function} label-inline - The field label, if label position is 'inline'.
+ * @param {function} default - Debug information if Nuxt is in development mode.
+ */
 </script>
