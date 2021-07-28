d0c833dacfdbfac23e3cb2272dfd0f90940a7aad
Decipher <stu@rtclark.net>
2021-07-28 14:55:01 +1000

    [82] chore: add druxt-test-utils package.

---
+++ packages/test-utils/src/mocks.js
@@ -0,0 +1,18 @@
+import { DrupalJsonApiParams } from 'drupal-jsonapi-params'
+import { DruxtClient } from 'druxt'
+import mockAxios from 'jest-mock-axios'
+
+const druxt = new DruxtClient('https://demo-api.druxtjs.org')
+
+export const getMockCollection = async (resourceType, query) => {
+  const collection = await druxt.getCollection(resourceType, query)
+  mockAxios.reset()
+  return collection
+}
+
+export const getMockResource = async (resourceType, query) => {
+  const collection = await getMockCollection(resourceType, new DrupalJsonApiParams().addFields(resourceType, []).addPageLimit(1))
+  const resource = await druxt.getResource(resourceType, collection.data[0].id, query)
+  mockAxios.reset()
+  return resource
+}
