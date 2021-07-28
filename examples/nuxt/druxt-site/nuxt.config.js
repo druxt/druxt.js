222b72acd3a464ba3a73791dfef5ec71226cde2c
Decipher <stu@rtclark.net>
2021-07-28 18:50:41 +1000

    [#82] chore: update paths.

--- examples/nuxt/druxt-site/nuxt.config.js
+++ examples/nuxt/druxt-site/nuxt.config.js
@@ -2,6 +2,7 @@ const baseUrl = process.env.BASE_URL || 'https://demo-api.druxtjs.org'
 export default {
   target: 'static',
   generate: { routes: ['/'] },
+  telemetry: true,
   modules: ['druxt-site'],
   druxt: { baseUrl },
   proxy: [baseUrl + '/sites/default/files']
