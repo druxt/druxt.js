b103f5497ffc49d12f3886a5f549f9c56c758c7a
Decipher <stu@rtclark.net>
2021-07-28 18:07:26 +1000

    [#82] chore: update druxt-site command.

--- examples/nuxt/druxt-site/nuxt.config.js
+++ examples/nuxt/druxt-site/nuxt.config.js
@@ -1,7 +1,8 @@
+const baseUrl = process.env.BASE_URL || 'https://demo-api.druxtjs.org'
 export default {
   target: 'static',
   generate: { routes: ['/'] },
   modules: ['druxt-site'],
-  druxt: { baseUrl: 'http://localhost:8080' },
-  proxy: ['http://localhost:8080/sites/default/files']
+  druxt: { baseUrl },
+  proxy: [baseUrl + '/sites/default/files']
 }
