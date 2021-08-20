5e4d927ac1a7c0261b5b64fb622080529be6c379
Stuart Clark <stu@rtclark.net>
2021-08-20 11:43:22 +0000

    feat(#150): add API docs NuxtContent page

---
+++ docs/pages/api/_.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtContent :document="document" />
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    let document
+    try {
+      document = await $content('api/', params.pathMatch || 'index').fetch();
+    } catch (e) {
+      return error({ message: 'Document not found' })
+    }
+
+    return {
+      document
+    }
+  },
+}
+</script>
