3dbcdb33975371153a9f7aafe3c05f8e04d7ee7b
Decipher <stu@rtclark.net>
2021-08-22 12:28:59 +1000

    chore(#150): add DaisyUI

---
+++ docs/windicss.config.js
@@ -0,0 +1,41 @@
+const { transform } = require('windicss/helpers')
+
+module.exports = {
+  plugins: [
+    transform('daisyui'),
+  ],
+
+  daisyui: {
+    themes: [
+      {
+        druxt: {
+          'primary': '#3498DB',           /* Primary color */
+          'primary-focus': '#2F495E',     /* Primary color - focused */
+          'primary-content': '#ffffff',   /* Foreground content color to use on primary color */
+
+          'secondary': '#108775',         /* Secondary color */
+          'secondary-focus': '#2F495E',   /* Secondary color - focused */
+          'secondary-content': '#ffffff', /* Foreground content color to use on secondary color */
+
+          'accent': '#37cdbe',            /* Accent color */
+          'accent-focus': '#2aa79b',      /* Accent color - focused */
+          'accent-content': '#ffffff',    /* Foreground content color to use on accent color */
+
+          'neutral': '#3d4451',           /* Neutral color */
+          'neutral-focus': '#2a2e37',     /* Neutral color - focused */
+          'neutral-content': '#ffffff',   /* Foreground content color to use on neutral color */
+
+          'base-100': '#ffffff',          /* Base color of page, used for blank backgrounds */
+          'base-200': '#f9fafb',          /* Base color, a little darker */
+          'base-300': '#d1d5db',          /* Base color, even more darker */
+          'base-content': '#1f2937',      /* Foreground content color to use on base color */
+
+          'info': '#2094f3',              /* Info */
+          'success': '#009485',           /* Success */
+          'warning': '#ff9900',           /* Warning */
+          'error': '#ff5724',             /* Error */
+        }
+      }
+    ]
+  }
+}
