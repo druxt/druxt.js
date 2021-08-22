3d58a5998c0466dbf80452921d0065d30cbed909
Decipher <stu@rtclark.net>
2021-08-22 12:30:16 +1000

    chore(#150): add WIP components

---
+++ docs/components/app/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="w-full navbar">
+    <div class="flex-none">
+      <label for="sidebar" class="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="inline-block w-6 h-6 stroke-current"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          ></path>
+        </svg>
+      </label>
+    </div>
+    <AppLogo class="flex-none h-8 mr-3" />
+    <div class="flex-1 px-2 mx-2">
+      <span class="text-lg font-bold">{{ title }}</span>
+    </div>
+    <div class="flex-none hidden lg:block">
+      <AppMenu class="horizontal" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ docs/pages/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="bg-gray-100 border-b text-center">
+      <div class="container mx-auto py-15">
+        <AppLogo class="w-1/4 mx-auto" />
+        <h1 class="text-6xl my-10" v-text="title" />
+        <h2 class="text-3xl my-5" v-text="slogan" />
+        <button class="btn btn-primary btn-lg">Get started</button>
+      </div>
+    </div>
+
+    <div class="container flex flex-col lg:flex-row mx-auto my-10">
+      <div
+        v-for="(block, index) of blocks"
+        :key="index"
+        class="flex-1 h-32 card"
+      >
+        <h4 class="text-2xl mb-3" v-text="block.title" />
+        <p class="text-lg" v-text="block.content" />
+      </div>
+    </div>
+
+    <div class="bg-neutral p-4 text-neutral-content">
+      <blockquote class="container mx-auto text-2xl" v-text="quote" />
+    </div>
+
+    <div>
+      <h3>Title</h3>
+      <p>Text</p>
+    </div>
+
+    <div>
+      <h3>Title</h3>
+      <p>Text</p>
+    </div>
+
+    <div>
+      <h3>Title</h3>
+      <p>Text</p>
+    </div>
+
+    <div>
+      <h3>Title</h3>
+      <p>Text</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: "DruxtJS",
+    slogan:
+      "A Bridge between frameworks, Nuxt.js in the front, Drupal in the back.",
+    blocks: [
+      {
+        title: "Simplicity first",
+        content:
+          "The Druxt component connects Nuxt.js to your Drupal JSON:API.",
+      },
+      {
+        title: "Community built",
+        content: "Built by the community, for the community.",
+      },
+      {
+        title: "Decoupled Drupal sites",
+        content:
+          "The Site module brings decoupled Vue.js theming system to Drupal.",
+      },
+    ],
+    quote:
+      "DruxtJS provides an easy connection between a Drupal JSON:API backend and Nuxt.js frontend application.",
+  }),
+};
+</script>
