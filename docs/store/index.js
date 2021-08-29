2d4f933e73694de27e9c50982b16b6f524fdef50
Stuart Clark <stu@rtclark.net>
2021-08-29 00:41:29 +0000

    chore(#150): fix missing path

--- docs/store/index.js
+++ docs/store/index.js
@@ -2,7 +2,7 @@ export const actions = {
   async nuxtServerInit ({ commit }, { $content }) {
     try {
       // Add Guide menu children to the vuex store.
-      const apiIndex = await $content("api").only(["title"]).fetch();
+      const apiIndex = await $content("api").only(["path", "title"]).fetch();
       const apiChildren = apiIndex.map((o) => ({
         component: "NuxtLink",
         text: o.title,
@@ -11,7 +11,7 @@ export const actions = {
       commit("addMenuChildren", { children: apiChildren, parent: "/api" });
 
       // Add Guide menu children to the vuex store.
-      const guideIndex = await $content("guide").only(["title"]).fetch();
+      const guideIndex = await $content("guide").only(["path", "title"]).fetch();
       const guideChildren = guideIndex.map((o) => ({
         component: "NuxtLink",
         text: o.title,
