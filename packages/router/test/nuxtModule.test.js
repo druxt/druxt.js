927dc3b8d33e72889c39eac0a7c18cac97f05f1f
Stuart Clark <stu@rtclark.net>
2021-09-15 05:12:15 +0000

    chore(#131): add test coverage

--- packages/router/test/nuxtModule.test.js
+++ packages/router/test/nuxtModule.test.js
@@ -9,15 +9,32 @@ const mock = {
 
     func(routes, resolve)
   }),
+  nuxt: {
+    hook: jest.fn((hook, callback) => callback()),
+    options: { build: {} }
+  },
   DruxtRouterNuxtModule
 }
 
 test('Nuxt module', () => {
   expect(() => { mock.DruxtRouterNuxtModule() }).toThrow('Druxt settings missing.')
 
-  mock.options = {
-    druxt: {}
-  }
+  mock.options = { druxt: {} }
   mock.DruxtRouterNuxtModule()
-  expect(mock.addPlugin).toHaveBeenCalled()
+  expect(mock.addPlugin).toHaveBeenCalledTimes(3)
+  expect(mock.nuxt.hook).toHaveBeenCalledTimes(0)
+  jest.clearAllMocks()
+
+  mock.options.druxt.router = { ignorePages: true }
+  mock.DruxtRouterNuxtModule()
+  expect(mock.addPlugin).toHaveBeenCalledTimes(3)
+  expect(mock.nuxt.hook).toHaveBeenCalledTimes(1)
+  expect(mock.nuxt.options.build.createRoutes()).toStrictEqual([])
+  jest.clearAllMocks()
+
+  mock.options.druxt.router = { wildcardRoute: false }
+  mock.DruxtRouterNuxtModule()
+  expect(mock.addPlugin).toHaveBeenCalledTimes(2)
+  expect(mock.nuxt.hook).toHaveBeenCalledTimes(0)
+  jest.clearAllMocks()
 })
