38bc2f8f4e6f67c727eb038b32bacd37f3717d22
Brian Gilbert <brian@realityloop.com>
2021-10-18 04:11:19 +0000

    chore: update blog layout for homepage

--- docs/components/app/Blog.vue
+++ docs/components/app/Blog.vue
@@ -1,24 +1,21 @@
 <template>
   <div>
-    <div class="hero min-h-full">
-      <div class="flex-col hero-content lg:flex-row-reverse">
-        <img src="https://picsum.photos/id/1005/600/600" class="max-w-sm rounded-lg shadow-2xl">
-        <div>
-          <h1 class="mb-5 text-5xl font-bold">
-                Hello there
-              </h1>
-          <p class="mb-5">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-              </p>
+    <div class="card container mx-auto my-8 lg:card-side bordered">
+      <figure>
+        <img src="https://picsum.photos/id/1005/400/250">
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Horizontal
+          <div class="badge mx-2">NEW</div>
+        </h2>
+        <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
+        <div class="card-actions">
           <button class="btn btn-primary">Read more</button>
         </div>
       </div>
     </div>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 container mx-auto my-8">
       <div class="card compact shadow-sm bg-accent text-accent-content">
-        <figure>
-          <img src="https://picsum.photos/id/1005/400/250">
-        </figure>
         <div class="card-body">
           <h2 class="card-title">Accent color</h2>
           <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
@@ -28,9 +25,6 @@
         </div>
       </div>
       <div class="card compact shadow-xl image-full">
-        <figure>
-          <img src="https://picsum.photos/id/1005/400/250">
-        </figure>
         <div class="justify-end card-body">
           <h2 class="card-title">Image overlay</h2>
           <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
@@ -40,9 +34,6 @@
         </div>
       </div>
       <div class="card compact shadow-sm bg-accent text-accent-content">
-        <figure>
-          <img src="https://picsum.photos/id/1005/400/250">
-        </figure>
         <div class="card-body">
           <h2 class="card-title">Accent color</h2>
           <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
@@ -52,9 +43,6 @@
         </div>
       </div>
       <div class="card compact shadow-xl image-full">
-        <figure>
-          <img src="https://picsum.photos/id/1005/400/250">
-        </figure>
         <div class="justify-end card-body">
           <h2 class="card-title">Image overlay</h2>
           <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
