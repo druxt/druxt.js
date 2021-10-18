b57411f5930bf137c1e605fee7641c22f3d51b0a
Brian Gilbert <brian@realityloop.com>
2021-10-18 04:04:14 +0000

    chore: update homepage blog layout

--- docs/components/app/Blog.vue
+++ docs/components/app/Blog.vue
@@ -1,33 +1,33 @@
 <template>
   <div>
-    <div class="card container mx-auto my-8 lg:card-side bordered">
-      <figure>
-        <img src="https://picsum.photos/id/1005/400/250">
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Horizontal
-          <div class="badge mx-2">NEW</div>
-        </h2>
-        <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
-        <div class="card-actions">
+    <div class="hero min-h-full">
+      <div class="flex-col hero-content lg:flex-row-reverse">
+        <img src="https://picsum.photos/id/1005/600/600" class="max-w-sm rounded-lg shadow-2xl">
+        <div>
+          <h1 class="mb-5 text-5xl font-bold">
+                Hello there
+              </h1>
+          <p class="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+              </p>
           <button class="btn btn-primary">Read more</button>
         </div>
       </div>
     </div>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 container mx-auto my-8">
-      <div class="card text-center shadow-2xl">
-        <figure class="px-10 pt-10">
-          <img src="https://picsum.photos/id/1005/400/250" class="rounded-xl">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 container mx-auto my-8">
+      <div class="card compact shadow-sm bg-accent text-accent-content">
+        <figure>
+          <img src="https://picsum.photos/id/1005/400/250">
         </figure>
         <div class="card-body">
-          <h2 class="card-title">shadow, center, padding</h2>
+          <h2 class="card-title">Accent color</h2>
           <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
-          <div class="justify-center card-actions">
-            <button class="btn btn-outline btn-accent">More info</button>
+          <div class="card-actions">
+            <button class="btn btn-secondary">More info</button>
           </div>
         </div>
       </div>
-      <div class="card shadow-xl image-full">
+      <div class="card compact shadow-xl image-full">
         <figure>
           <img src="https://picsum.photos/id/1005/400/250">
         </figure>
@@ -39,7 +39,7 @@
           </div>
         </div>
       </div>
-      <div class="card shadow-sm bg-accent text-accent-content">
+      <div class="card compact shadow-sm bg-accent text-accent-content">
         <figure>
           <img src="https://picsum.photos/id/1005/400/250">
         </figure>
@@ -48,6 +48,18 @@
           <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
           <div class="card-actions">
             <button class="btn btn-secondary">More info</button>
+          </div>
+        </div>
+      </div>
+      <div class="card compact shadow-xl image-full">
+        <figure>
+          <img src="https://picsum.photos/id/1005/400/250">
+        </figure>
+        <div class="justify-end card-body">
+          <h2 class="card-title">Image overlay</h2>
+          <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p>
+          <div class="card-actions">
+            <button class="btn btn-primary">Get Started</button>
           </div>
         </div>
       </div>
