f60d6bc91a6ef2f1c353ba2012bc92cbb3130729
Stuart Clark <stu@rtclark.net>
2021-11-02 01:03:50 +0000

    chore(#355): add Entity Queries examples

---
+++ examples/nuxt/entity-queries/components/druxt/entity/node/Article.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  druxt: {
+    query: {
+      // Filter only Schema fields.
+      schema: true,
+    }
+  }
+}
+</script>
