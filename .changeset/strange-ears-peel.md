85fff42fdb7483ad0993eb7fe2f5341349ccbf77
Stuart Clark <stu@rtclark.net>
2021-12-01 16:30:39 +1100

    feat(#69): add debug mode and axios logging (#388)
    * feat(#69): add debug mode and axios logging
    
    * chore(#69): add consola as peer dependency
    
    * chore(#69): add test
    
    * chore(#69): update documentation

---
+++ .changeset/strange-ears-peel.md
@@ -0,0 +1,14 @@
+---
+"druxt": minor
+---
+
+Added debug mode and Axios request logging.
+
+```
+export default {
+  druxt: {
+    // Enable debug log messages.
+    debug: true,
+  }
+}
+```
--- packages/druxt/src/client.js
+++ packages/druxt/src/client.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { stringify } from 'querystring'
+import consola from 'consola'
 
 /**
  * The Druxt JSON:API client.
@@ -20,6 +21,11 @@ class DruxtClient {
    * @param {DruxtClientOptions} [options] - The DruxtClient options object.
    */
   constructor(baseUrl, options = {}) {
+    // Consola logger.
+    this.log = consola.create({ defaults: {
+      tag: 'DruxtClient'
+    }})
+
     // Check for URL.
     if (!baseUrl) {
       throw new Error('The \'baseUrl\' parameter is required.')
@@ -41,6 +47,19 @@ class DruxtClient {
      * @type {object}
      */
     this.axios = axios.create(axiosSettings)
+
+    // If Debug mode is enabled, add an Axios interceptor to log out all
+    // requests and errors.
+    if (options.debug) {
+      const log = this.log
+      this.axios.interceptors.request.use((config) => {
+        log.info(config.url)
+        return config
+      }, (error) => {
+        log.error(error)
+        return Promise.reject(error)
+      })
+    }
 
     /**
      * Druxt base options.
@@ -398,6 +417,7 @@ export { DruxtClient }
   * @typedef {object} DruxtClientOptions
   *
   * @param {object} [axios] - Axios instance settings.
+  * @param {boolean} [debug=false] - Enable Debug mode for verbose console log messages.
   * @param {string} [endpoint=jsonapi] - The JSON:API endpoint.
   * @param {string} [jsonapiResourceConfig=jsonapi_resource_config--jsonapi_resource_config] -
   *   The JSON:API resource config type, used for [JSON:API Extras](https://www.drupal.org/project/jsonapi_extras) support.
@@ -406,9 +426,14 @@ export { DruxtClient }
   *
   * @example @lang js
   * {
+  *   // Axios instance settings.
   *   axios: {
+  *     // Set axios headers.
   *     headers: { 'X-Custom-Header': true },
   *   },
+  *   // Enable debug console log messages.
+  *   debug: true,
+  *   // JSON:API endpoint.
   *   endpoint: 'api',
   * }
   */
