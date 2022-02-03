4a888fd96066b6619d3d9b58375ba6eebcad0e97
Stuart Clark <stu@rtclark.net>
2022-02-03 13:50:47 +1100

    chore(#420): update ddev to 1.18.2

--- .gitpod/scripts/drupal-setup.sh
+++ .gitpod/scripts/drupal-setup.sh
@@ -9,14 +9,6 @@ set -eu -o pipefail
 
 DRUPAL_DIR="${GITPOD_REPO_ROOT}/examples/drupal/drupal-9"
 
-# Set up DDEV
-cd "$GITPOD_REPO_ROOT" && .gitpod/scripts/ddev-setup.sh
-
 # Set up Drupal website
 cd "$DRUPAL_DIR" && ddev start
-cd "$DRUPAL_DIR" && ddev composer install
-cd "$DRUPAL_DIR" && ddev drush si -y --account-pass=admin demo_umami
-cd "$DRUPAL_DIR" && ddev drush -y en druxt
-cd "$DRUPAL_DIR" && ddev drush "rap anonymous 'access druxt resources'"
-cd "$DRUPAL_DIR" && ddev drush -y config:set jsonapi.settings read_only 0
-cd "$DRUPAL_DIR" && ddev stop
+cd "$DRUPAL_DIR" && ddev drupal-install
