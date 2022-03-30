c503f13ac737261238a93dbf5ac75770595e1ed1
Stuart Clark <stu@rtclark.net>
2022-03-30 03:39:52 +0000

    chore(#158): add multilingual examples

--- examples/node/client/index.js
+++ examples/node/client/index.js
@@ -11,14 +11,40 @@ druxt.getResource('node--page', 'cd44fe14-86ae-4853-8e22-7b1b73cd98f5', 'fields[
   console.log('getResource', resource)
 })
 
+// Get page in spanish.
+druxt.getResource('node--page', 'cd44fe14-86ae-4853-8e22-7b1b73cd98f5', 'fields[node--page]=title', 'es').then((resource) => {
+  console.log('getResource', resource)
+})
+
 // Get a collection of recipes.
 druxt.getCollection('node--recipe').then((collection) => {
-  console.log('getCollection', collection)
+  for (let i = 0; i <= collection.data.length; i++) {
+    console.log('getCollection', i, collection.data[i])
+  }
+})
+
+// Get a collection of spanish recipes.
+druxt.getCollection('node--recipe', undefined, 'es').then((collection) => {
+  for (let i = 0; i <= collection.data.length; i++) {
+    console.log('getCollection', i, collection.data[i])
+  }
 })
 
 // Get all Recipe nodes.
 druxt.getCollectionAll('node--recipe').then(collections => {
   console.log('getCollectionAll')
+  for (i in collections) {
+    const collection = collections[i]
+    for (j in collection.data) {
+      const resource = collection.data[j]
+      console.log(resource.id, resource.attributes.title)
+    }
+  }
+})
+
+// Get all spanish Recipe nodes.
+druxt.getCollectionAll('node--recipe', undefined, 'es').then(collections => {
+  console.log('getCollectionAll - Spanish')
   for (i in collections) {
     const collection = collections[i]
     for (j in collection.data) {
