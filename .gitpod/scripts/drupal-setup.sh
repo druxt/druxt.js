43f33373ba1545c514ddc4c48424ea95331d8f20
Stuart Clark <stu@rtclark.net>
2022-04-18 22:57:36 +0000

    chore: add -y flag to ddev start command

--- .gitpod/scripts/drupal-setup.sh
+++ .gitpod/scripts/drupal-setup.sh
@@ -10,5 +10,5 @@ set -eu -o pipefail
 DRUPAL_DIR="${GITPOD_REPO_ROOT}/examples/drupal/drupal-9"
 
 # Set up Drupal website
-cd "$DRUPAL_DIR" && ddev start
+cd "$DRUPAL_DIR" && ddev start -y
 cd "$DRUPAL_DIR" && ddev drupal-install
