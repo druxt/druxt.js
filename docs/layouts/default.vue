94fd55faa2541813117a86293cb1ea444db16d19
Dave Connerth <captaindav@gmail.com>
2022-04-29 19:02:12 +0000

    docs: added full text search component FullTextSearch.vue and search field in default.vue (#197)

--- docs/layouts/default.vue
+++ docs/layouts/default.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="flex flex-col h-screen">
-    <AppHeader class="inset-x-0 top-0 z-50 w-full sticky" title="DruxtJS" />
+    <AppHeader
+      class="inset-x-0 top-0 z-50 w-full sticky"
+      title="DruxtJS"
+    />
+    <FullTextSearch />
+
+    <div>
+      <input
+        v-model="searchQuery"
+        type="search"
+        autocomplete="off"
+      >
+
+      <ul v-if="results.length">
+        <li
+          v-for="result of results"
+          :key="result.key"
+        >
+          <router-link :to="result.path">
+            {{ result.title }}
+          </router-link>
+        </li>
+      </ul>
+    </div>
 
     <div
-      class="shadow bg-base-200 drawer flex-grow"
+      class="shadow bg-basr aw- grow"
       :class="{ 'drawer-mobile': $route.path !== '/' }"
     >
       <input
         id="sidebar"
+        v-model="sidebar"
         type="checkbox"
         class="drawer-toggle"
-        v-model="sidebar"
-      />
+      >
 
       <main
         ref="main"
@@ -28,7 +51,10 @@
       </main>
 
       <div class="drawer-side">
-        <label for="sidebar" class="drawer-overlay"></label>
+        <label
+          for="sidebar"
+          class="drawer-overlay"
+        />
         <AppSidebar />
       </div>
     </div>
@@ -39,16 +65,50 @@
 export default {
   data: () => ({
     sidebar: false,
+    searchQuery: "",
+    results: [],
   }),
 
   watch: {
     $route() {
       // Close the sidebar.
-      this.sidebar = false
+      this.sidebar = false;
 
       // Scroll to the top of the main div.
-      this.$refs.main.scrollTop = 0
-    }
-  }
-}
+      this.$refs.main.scrollTop = 0;
+    },
+    async searchQuery(searchQuery) {
+      if (!searchQuery) {
+        this.results = [];
+        return;
+      }
+      var newResults = [];
+      newResults = await this.$content("guide")
+        .sortBy("createdAt", "asc")
+        .limit(100)
+        .search(searchQuery)
+        .fetch();
+
+      newResults.forEach(function (result, index) {
+        //result.path += result.extension
+        //console.log(item, index);
+      })
+
+      console.log(newResults[1]);
+      // newResults.push( await this.$content('guide')
+      //   .sortBy('createdAt', 'asc')
+      //   .limit(100)
+      //   .search(searchQuery)
+      //   .fetch())
+
+      // newResults.push( await this.$content('module')
+      //   .sortBy('createdAt', 'asc')
+      //   .limit(100)
+      //   .search(searchQuery)
+      //   .fetch())
+      this.results = newResults;
+      console.log(newResults[1]);
+    },
+  },
+};
 </script>
