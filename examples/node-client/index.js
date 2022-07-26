985cf0c615bdb306553f56db6ceb0f999ef7997d
Stuart Clark <stu@rtclark.net>
2022-07-26 01:43:45 +0000

    chore(#542): update ddev config and backend id

--- examples/node-client/index.js
+++ examples/node-client/index.js
@@ -2,7 +2,7 @@ const { DruxtClient } = require('druxt')
 
 const baseUrl = process.env.GITPOD_WORKSPACE_ID
   ? `https://8080-${process.env.GITPOD_WORKSPACE_ID}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`
-  : process.env.BASE_URL || 'http://drupal-9.ddev.site'
+  : process.env.BASE_URL || 'http://druxtjs.ddev.site'
 
 const druxt = new DruxtClient(baseUrl)
 
