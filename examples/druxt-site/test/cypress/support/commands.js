a3cc102db3a4c323e41b2216ccb21b69e02d029d
Dave Connerth <captaindav@gmail.com>
2022-08-16 19:35:40 -0400

    chore(#92): added login command

--- examples/druxt-site/test/cypress/support/commands.js
+++ examples/druxt-site/test/cypress/support/commands.js
@@ -23,3 +23,43 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", ({ username = 'admin' }, { password = 'admin' }, { rememberUser = false } = {}) => {
+
+  const signInUrl = "http://localhost:3000"
+  const signInPath = "/user/login"
+  cy.visit(signInUrl + signInPath)
+
+  const log = Cypress.log({
+    name: "login",
+    displayName: "LOGIN",
+    message: [`🔐 Authenticating | ${username}`],
+    // @ts-ignore
+    autoEnd: false,
+  });
+
+  log.snapshot("before");
+
+  // Fill out login form fields.
+  cy.get("#edit-name").type(username);
+  cy.get("edit-pass").type(password);
+
+  // Submit login form.
+  cy.get("edit-submit").click();
+
+  cy.wait("@loginUser").then((loginUser: any) => {
+    log.set({
+      consoleProps() {
+        return {
+          username,
+          password,
+          // userId: loginUser.response.statusCode !== 401 && loginUser.response.body.user.id,
+        };
+      },
+    });
+
+    log.snapshot("after");
+    log.end();
+  });
+});
+
