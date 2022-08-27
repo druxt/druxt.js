8ec9405bf2d1073b8bf3f5d21192d31db7d059f5
Stuart Clark <stu@rtclark.net>
2022-08-27 17:42:29 +1000

    chore(#92): move cypress to root package

--- examples/druxt-site/test/cypress.config.js
+++ examples/druxt-site/test/cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000'
   },
 });
