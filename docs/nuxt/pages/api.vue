db1f11d7b318455276f18e8640f7c1148afddb56
Stuart Clark <stu@rtclark.net>
2022-10-06 12:39:11 +1100

    chore(#542): update nuxt children

--- docs/nuxt/pages/api.vue
+++ docs/nuxt/pages/api.vue
@@ -2,6 +2,6 @@
   <div class="container mx-auto my-20 px-5">
     <h1 class="text-4xl text-primary border-b mb-10">API Documentation</h1>
 
-    <NuxtChild />
+    <NuxtChild keep-alive :key="$route.fullPath" />
   </div>
 </template>
--- docs/nuxt/pages/guide.vue
+++ docs/nuxt/pages/guide.vue
@@ -2,6 +2,6 @@
   <div class="container mx-auto my-20 px-5">
     <h1 class="text-4xl text-secondary border-b mb-10">Guide</h1>
 
-    <NuxtChild />
+    <NuxtChild keep-alive :key="$route.fullPath" />
   </div>
 </template>
