2517dbd37603d3718f8a8ca3e290db2bbe1ded6d
Stuart Clark <stu@rtclark.net>
2022-10-25 13:44:28 +1100

    chore(#578): add test coverage for template tool

---
+++ examples/druxt-site/test/cypress/e2e/nuxt/druxt-devel-template.cy.js
@@ -0,0 +1,34 @@
+it('DruxtDevelTemplate tool', () => {
+  // Given I visit the homepage.
+  cy.visit('/')
+
+  // Umami Search block.
+  const searchBlock = '[data-fetch-key^="DruxtBlock:dc9cc806-d5d5-4187-af7a-f9d5b13fea8b:0"]'
+
+  // I see a DruxtDebug component.
+  cy.get(searchBlock)
+    .find('details summary')
+    .first()
+    .should('contain.text', "[DruxtBlock] Missing Vue template for the 'umami_search' block")
+    .click()
+
+  // It has 5 theme component options.
+  cy.get(searchBlock)
+    .find('select option')
+    .should('have.length', 5)
+
+  // I create the DruxtBlockSearchFormBlock component
+  cy.get(searchBlock)
+    .find('select')
+    .select('DruxtBlockSearchFormBlock')
+  cy.get(searchBlock)
+    .find('button')
+    .click()
+
+  cy.wait(3000)
+  cy.get(searchBlock)
+    .find('details summary')
+    .first()
+    .should('contain.text', "[DruxtBlockSearchBlockForm] Debug")
+    .click()
+})
