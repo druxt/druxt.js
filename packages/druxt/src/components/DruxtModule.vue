9d34bff5678c277c417658177efb67deedff5d57
Stuart Clark <stu@rtclark.net>
2022-10-26 22:42:08 +0000

    chore(#578): update slot message and description

--- packages/druxt/src/components/DruxtModule.vue
+++ packages/druxt/src/components/DruxtModule.vue
@@ -384,15 +384,15 @@ export default {
       // Provide debug data if Nuxt is running in dev mode.
       if (!scopedSlots.default && this.$nuxt.context.isDev)  {
         if (!scopedSlots.debug) {
-          const summary = `Module does not provide a default slot.`
-          const description = [h('p', `The "${this.$options._componentTag}" module does not provide a default slot.`), h('p', 'Create a Druxt theme component to render the data as required.')]
+          const summary = 'Missing default slot'
+          const description = [h('p', `The "${this.$options._componentTag}" component does not provide a default slot.`), h('p', 'Create a Druxt theme component to render the data as required.')]
           scopedSlots.debug = () => h(
             'DruxtDebug',
             { props: { summary } },
             [
               h('div', description),
               this.component.is === 'DruxtWrapper' && !!this.component.options.length && h('DruxtDevelTemplate', { props: { options: this.component.options }}),
-              h('details', [h('summary', 'Data'), h('pre', [h('code', [JSON.stringify(this.model, null, '  ')])])])
+              h('details', [h('summary', 'Data'), h('pre', [h('code', [JSON.stringify(this.component.propsData, null, '  ')])])])
             ]
           )
         }
