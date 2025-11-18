7d7316309cd618c45e033c8a4add231623d5df12
Stuart Clark <stu@rtclark.net>
2025-11-18 13:37:35 +1100

    feat: add prettier for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,11 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:nuxt/recommended',
-    'plugin:vue/recommended'
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended'
   ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
 }
